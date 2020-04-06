966cd6d42388ce688658cfeca9a7007c49c6413a
Masyanyaz <47759559+Masyanyaz@users.noreply.github.com>
2020-04-06 23:43:58 +0300

    asdas

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1247311435:AAFyIcmkgq0Z5efLjylJeURftBqAMuNWlkk';
+const token = '1247311435:AAGJySOJzjXpAjT_BP30oQEGf5Vqhpxdm4o';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+bot.onText(/\/start (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, `Привет ${msg.from.first_name}. Чем могу помочь?`);
+});
 
 // Listen for any kind of message. There are different kinds of
 // messages.
@@ -14,5 +37,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Привет,' + msg.from.first_name + ', Как дела?');
+  bot.sendMessage(chatId, `Я еще не знаю этой команды(((`);
 });
