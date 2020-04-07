9d8a83bbe41a579fda10558cc362a74d12ec781a
Masyanyaz <47759559+Masyanyaz@users.noreply.github.com>
2020-04-07 10:28:28 +0300

    asdas

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 process.env.NTBA_FIX_319 = 1;
-
+const port = process.env.PORT || 8443;
+const host = process.env.HOST;
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '1247311435:AAGJySOJzjXpAjT_BP30oQEGf5Vqhpxdm4o';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true, webHook: {port: port, host: host}});
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -20,7 +21,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
-bot.onText(/\/start (.+)/, (msg, match) => {
+bot.onText(/\/start/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
