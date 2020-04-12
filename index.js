075597409b77baeea8bb4e3f62d37887f0afd6be
Masyanyaz <47759559+Masyanyaz@users.noreply.github.com>
2020-04-13 01:45:46 +0300

    'asdas'

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM films;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '1247311435:AAGJySOJzjXpAjT_BP30oQEGf5Vqhpxdm4o';
@@ -21,44 +36,11 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-// Matches "/echo [whatever]"
-bot.onText(/\+ (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-  if (resp)
-    fs.writeFile(`${msg.chat.id}.txt`, `${resp}\r\n`, {flag: 'a+'}, err => {
-      if (err) console.error(err)
-    })
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, `${resp} сохранен`);
-});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/list/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-
-  fs.readFile(`${msg.chat.id}.txt`, 'utf8', function(err, contents) {
-    bot.sendMessage(chatId, contents);
-  });
-
-  // send back the matched "whatever" to the chat
-
-});
-
 bot.onText(/\/start/, (msg) => {
 
   bot.sendMessage(msg.chat.id, `Привет ${msg.from.first_name}. Чем могу помочь?`, {
     "reply_markup": {
-      "keyboard": [["/list", "Second sample"], ["Keyboard"], ["I'm robot"]]
+      "keyboard": [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]]
     }
   });
 
